680714d92b6ee981667b1885b7012f68229d847e
JungWKim <lionmini@naver.com>
2021-01-11 22:30:53 +0900

    first commit

---
+++ src/ex0111/method/Hello.java
@@ -0,0 +1,14 @@
+package ex0111.method;
+
+public class Hello {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("자바 시작하기");
+		byte a = 2;
+		byte b = 3;
+		byte c = (byte)(a + b);
+		System.out.format("result : %d", (a + b));
+	}
+
+}
---
+++ src/ex0111/method/test/A.java
@@ -0,0 +1,13 @@
+package ex0111.method.test;
+
+class A {
+
+}
+
+class B {
+	
+}
+
+class C {
+	
+}
